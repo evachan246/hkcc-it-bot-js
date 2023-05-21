67ae213e5242fa054609236ee7ea2f85800cf4f5
Eva Chan <evachan246@gmail.com>
2023-05-21 19:42:45 +0800

    Change the method of fetch cat photo

--- service/other/cat.js
+++ service/other/cat.js
@@ -1,11 +1,14 @@
 const fetch = require("node-fetch");
+const axios = require("axios");
 
 const cat = ( {bot, chatId, messageId} ) => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => response.json())
-        .then((json) => {
-            bot.sendPhoto(chatId, json[0]['url'])
-        });
+
+    axios.get("https://api.thecatapi.com/v1/images/search")
+        .then((res) => {
+            bot.sendPhoto(chatId, res.data[0]['url'])
+        })
+        .catch((error) => { console.error(error) })
+        .finally(() => { })
 }
 
 module.exports = {
